2bbd952f8ed8976d4c80757f3ceff1d97a42bb93
victor diaz <vdmpeniel@yahoo.es>
2023-06-29 00:00:26 -0400

    Making all required changes.

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,67 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    private String readFile(File file){
+        String content = "";
+        try(FileInputStream fileInputStream = new FileInputStream(file)){
+            int character;
+            while((character = fileInputStream.read()) != -1 ){
+                content += (char) character;
+            }
+
+        } catch(IOException ioe){
+            return ioe.getMessage();
+        }
+        return content;
     }
+
+    private void populateField(Profile profile, String field, String value){
+        try {
+            switch (field) {
+                case "Name":
+                    profile.setName(value);
+                    break;
+                case "Age":
+                    profile.setAge(Integer.valueOf(value));
+                    break;
+                case "Email":
+                    profile.setEmail(value);
+                    break;
+                case "Phone":
+                    profile.setPhone(Long.valueOf(value));
+                    break;
+                default:
+                    throw new IllegalArgumentException("Field " + field + "is not allowed.");
+            }
+        } catch(IllegalArgumentException iae){
+            System.out.println(iae.getMessage());
+        }
+    }
+
+    private String[] fields = {"Name", "Age", "Email", "Phone"};
+    private Profile parseContent(String content){
+        String[] lines = content.split("\r\n");
+
+        Profile profile = new Profile();
+        for(String str : lines){
+            for(String field : fields){
+                if(str.contains(field)){
+                    populateField(profile, field, str.replace(field + ": ", ""));
+                }
+            }
+        }
+        return profile;
+    }
+
+    public Profile getDataFromFile(File file) {
+        return parseContent(readFile(file));
+    }
+
 }
